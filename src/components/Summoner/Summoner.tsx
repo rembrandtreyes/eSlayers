80f02465f32edccbce8e1a72debef54135227598
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-07-22 00:23:54 -0700

    Create summoner component * Name * Rank info

---
+++ src/components/Summoner/Summoner.tsx
@@ -0,0 +1,30 @@
+import useSWR from "swr";
+
+import fetchSummoner from "../../utils/fetchSummoner";
+import fetchLeague from "../../utils/fetchLeague";
+
+const Summoner = ({ name }) => {
+  const summoner = fetchSummoner(name);
+
+  const league = fetchLeague(summoner.id);
+
+  const winRate = (league[0].wins / league[0].losses) * 100;
+
+  return (
+    <>
+      <div>{summoner && summoner.name}</div>
+      {league && (
+        <>
+          <h2>Tier: {league[0].tier}</h2>
+          <h2>Rank: {league[0].rank}</h2>
+          <h2>LP: {league[0].leaguePoints}</h2>
+          <h2>Wins: {league[0].wins}</h2>
+          <h2>Loses: {league[0].losses}</h2>
+          <h2>Win Rate: {winRate.toFixed(2)}%</h2>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Summoner;
