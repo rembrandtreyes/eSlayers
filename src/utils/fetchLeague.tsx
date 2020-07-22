761f3917982112348b151531e5a6aef63428911e
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-07-22 00:21:59 -0700

    Create util to fetch Leauge ranking

---
+++ src/utils/fetchLeague.tsx
@@ -0,0 +1,19 @@
+import useSWR from "swr";
+
+import fetcher from "./fetcher";
+
+const fetchLeague = (summonerId) => {
+  const { data: league, error } = useSWR(
+    summonerId
+      ? `/api/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+      : null,
+    fetcher
+  );
+
+  if (!league) return 'Loading...';
+  if (error) return 'Error loading';
+
+  return league;
+};
+
+export default fetchLeague;
