27c66caab93360798023a6f3fc1b75bae245c7c3
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-07-22 00:22:18 -0700

    Create util to fetch summoner info

---
+++ src/utils/fetchSummoner.tsx
@@ -0,0 +1,14 @@
+import useSWR from "swr";
+
+import fetcher from "./fetcher"
+
+const fetchSummoner = (name) => {
+  const { data: summoner, error } = useSWR(name ? `/api/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.NEXT_PUBLIC_API_KEY}` : null, fetcher);
+
+  if (!summoner) return 'Loading...';
+  if (error) return 'Error loading';
+
+  return summoner
+}
+
+export default fetchSummoner
