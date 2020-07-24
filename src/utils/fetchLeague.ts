c2784424fbcda2f77869124d23a3278e13f2c45f
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-07-23 22:04:48 -0700

    Modifiy `fetchLeague.ts` URL to the updated proxy API

--- src/utils/fetchLeague.ts
+++ src/utils/fetchLeague.ts
@@ -5,7 +5,7 @@ import fetcher from "./fetcher";
 const fetchLeague = (summonerId: number) => {
   const { data: league, error } = useSWR(
     summonerId
-      ? `/api/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+      ? `/na1/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
       : null,
     fetcher
   );
