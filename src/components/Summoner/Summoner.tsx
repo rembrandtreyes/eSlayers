c0f55e8558146033670d8e24100978b9b40bf550
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-07-23 22:02:39 -0700

    Modified `Summoner.tsx` to test newly fetched data

--- src/components/Summoner/Summoner.tsx
+++ src/components/Summoner/Summoner.tsx
@@ -1,5 +1,7 @@
 import fetchSummoner from "../../utils/fetchSummoner";
 import fetchLeague from "../../utils/fetchLeague";
+import fetchTftMatches from "../../utils/fetchTftMatches";
+import Match from "../Match";
 
 interface SummonerProps {
   name: string;
@@ -8,18 +10,20 @@ interface SummonerProps {
 const Summoner: React.FC<SummonerProps> = ({ name }) => {
   const summoner = fetchSummoner(name);
   const league = fetchLeague(summoner.id);
+  const matches = fetchTftMatches(summoner.puuid);
 
   const winRate = (league[0].wins / league[0].losses) * 100;
 
   return (
     <>
-      <div>{summoner && summoner.name}</div>
+      <h1>{summoner.name}</h1>
       <h2>Tier: {league[0].tier}</h2>
       <h2>Rank: {league[0].rank}</h2>
       <h2>LP: {league[0].leaguePoints}</h2>
       <h2>Wins: {league[0].wins}</h2>
       <h2>Loses: {league[0].losses}</h2>
       <h2>Win Rate: {winRate.toFixed(2)}%</h2>
+      {matches.length === 1 && matches.map((match) => <Match key={match} matches={match} />)}
     </>
   );
 };
