ac05794797608766f0b5d5ad89066d4d55740d22
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-07-23 22:06:42 -0700

    Modify `fetchSummoner.ts` URL to the updated proxy API
    Add in a new function to fetch summoner based on use `puuid`

--- src/utils/fetchSummoner.ts
+++ src/utils/fetchSummoner.ts
@@ -4,7 +4,7 @@ import fetcher from "./fetcher";
 
 const fetchSummoner = (name: string) => {
   const { data: summoner, error } = useSWR(
-    name ? `/api/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.NEXT_PUBLIC_API_KEY}` : null,
+    name ? `/na1/lol/summoner/v4/summoners/by-name/${name}?api_key=${process.env.NEXT_PUBLIC_API_KEY}` : null,
     fetcher
   );
 
@@ -12,6 +12,19 @@ const fetchSummoner = (name: string) => {
   if (error) return "Error loading";
 
   return summoner;
+};
+
+export const fetchSummonerPuuid = (puuid: string) => {
+  const { data, error } = useSWR(
+    puuid ? `/na1/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${process.env.NEXT_PUBLIC_API_KEY}` : null,
+    fetcher
+  );
+
+  return {
+    summonerPuuid: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 };
 
 export default fetchSummoner;
