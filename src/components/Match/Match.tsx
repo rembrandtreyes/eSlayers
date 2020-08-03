8b3bb9dd7c199ff0e88cba12d1cfcf1e7ed2d8e8
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-08-02 18:32:09 -0700

    Add more data to Match.tsx

--- src/components/Match/Match.tsx
+++ src/components/Match/Match.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import fetchTftMatch from "../../utils/fetchTftMatch";
 import Participant from "../Participant";
 
@@ -15,15 +17,38 @@ const Match: React.FC<MatchProps> = ({ matches }) => {
   const unixToDate = new Date(info.game_datetime * 1000);
   const humanDate = unixToDate.toLocaleString();
   const participants = info.participants;
+  console.log(participants);
 
   return (
     <div>
       <p>{humanDate}</p>
       {participants.map((participant) => (
-        <>
-          <Participant key={participant.puuid} puuid={participant.puuid} />
+        <React.Fragment key={participant.puuid}>
+          <h4>
+            <Participant puuid={participant.puuid} />
+          </h4>
           <p>Placement: {participant.placement}</p>
-        </>
+          <p>Gold Left: {participant.gold_left}</p>
+          <p>Level: {participant.level}</p>
+          <p>Players Eliminated: {participant.players_eliminated}</p>
+          <h5>Traits</h5>
+          {participant.traits.map((trait) => (
+            <div key={trait.name}>
+              <span>Trait Name: {trait.name}</span> | <span>Number of units: {trait.num_units}</span> |{" "}
+              <span>Tier Style: {trait.style}</span> | <span>Tier of trait: {trait.tier_current}</span>
+            </div>
+          ))}
+          <h5>Units</h5>
+          {participant.units.map((unit) => (
+            <div key={unit.character_id}>
+              <span>Unit name: {unit.character_id}</span> | <span>Tier: {unit.tier}</span> |{" "}
+              {unit.items.map((item) => (
+                <span>{item} </span>
+              ))}
+            </div>
+          ))}
+          <hr />
+        </React.Fragment>
       ))}
     </div>
   );
