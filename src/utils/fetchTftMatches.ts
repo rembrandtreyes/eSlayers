0cb7e2302765feb959d5a2b5038cb4bc787982ca
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-08-02 18:31:54 -0700

    Update fetchMatches

--- src/utils/fetchTftMatches.ts
+++ src/utils/fetchTftMatches.ts
@@ -3,17 +3,18 @@ import useSWR from "swr";
 import fetcher from "./fetcher";
 
 const fetchTftMatches = (puuid: string) => {
-  const { data: matches, error } = useSWR(() =>
+  const { data, error } = useSWR(
     puuid
-      ? `/americas/tft/match/v1/matches/by-puuid/${puuid}/ids?count=1&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+      ? `/americas/tft/match/v1/matches/by-puuid/${puuid}/ids?count=2&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
       : null,
     fetcher
   );
 
-  if (!matches) return "Loading...";
-  if (error) return "Error fetching matches";
-
-  return matches;
+  return {
+    matches: data,
+    isLoading: !error && !data,
+    isError: error
+  };
 };
 
 export default fetchTftMatches;
