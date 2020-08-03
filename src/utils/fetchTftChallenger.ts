cf7a0bb749c3185580214a19a1e42239a3048e92
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-08-02 19:35:07 -0700

    Add fetching for tft challenger leauge

---
+++ src/utils/fetchTftChallenger.ts
@@ -0,0 +1,15 @@
+import useSWR from "swr";
+
+import fetcher from "./fetcher";
+
+const fetchTftChallenger = () => {
+  const { data, error } = useSWR(`na1/tft/league/v1/challenger?api_key=${process.env.NEXT_PUBLIC_API_KEY}`, fetcher);
+
+  return {
+    challengerData: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
+
+export default fetchTftChallenger;
