ba5e0c569a6b2a0e71d6c80541a082a512be03b8
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-08-07 17:04:46 -0700

    Add Summoner Rank and Stats into its own component

---
+++ src/components/SummonerLeague/SummonerLeague.tsx
@@ -0,0 +1,21 @@
+import fetchLeague from "../../utils/fetchLeague";
+import fetchSummoner from "../../utils/fetchSummoner";
+
+const SummonerLeague = ({ name }) => {
+  const summoner = fetchSummoner(name);
+  const league = fetchLeague(summoner.id);
+  const winRate = (league[0].wins / league[0].losses) * 100;
+
+  return (
+    <div>
+      <h2>Tier: {league[0].tier}</h2>
+      <h2>Rank: {league[0].rank}</h2>
+      <h2>LP: {league[0].leaguePoints}</h2>
+      <h2>Wins: {league[0].wins}</h2>
+      <h2>Loses: {league[0].losses}</h2>
+      <h2>Win Rate: {winRate.toFixed(2)}%</h2>
+    </div>
+  );
+};
+
+export default SummonerLeague;
