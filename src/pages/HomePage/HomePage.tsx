cb799056004c54911116fdeeef0720ae6731829a
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-09-10 17:07:15 -0700

    Add some styles to HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,34 +1,53 @@
-import Router from "next/router";
-import { useState } from "react";
+import Router from "next/router"
+import { useState } from "react"
+import styled from "styled-components"
 
 import ChallengerLeague from "../../components/ChallengerLeague"
 
 const HomePage = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState("")
 
-  const handleSubmit = () => {
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault()
     Router.push({
       pathname: "/summoner",
       query: { name: input },
-    });
-  };
+    })
+  }
 
   return (
-    <div>
-      <div>
-        <img
-          src="https://res.cloudinary.com/rreyes/image/upload/v1596840707/6bca6cc9-800b-4c8b-a12a-6c38bd1acccd_200x200_f8evcp.png"
-          width="200"
-        />
-      </div>
-      <div>Search your summoner name</div>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-      <button type="submit" onClick={handleSubmit}>
-        Submit
-      </button>
+    <HomePageWrapper>
+      <Title>eSlayers</Title>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <h2>Search your username</h2>
+          <input
+            placeholder="Username"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </label>
+        {/* <input type="submit" value="Submit" /> */}
+        <button type="submit">Test</button>
+      </form>
       <ChallengerLeague />
-    </div>
-  );
+    </HomePageWrapper>
+  )
 }
+
+export const Title = styled.h1`
+  font-family: "Geostar Fill", cursive !important;
+  line-height: 1.5;
+  margin-bottom: 64px;
+`
+
+export const HomePageWrapper = styled.div`
+  margin: 112px 60px;
+`
+
+export const HomeForm = styled.form`
+  width: fit-content;
+`
 
 export default HomePage
