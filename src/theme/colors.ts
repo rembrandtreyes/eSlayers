86c5172d5b50f87161adf3a2bc39c789124c37c2
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-08 10:36:08 -0700

    WIP: Settling on a color scheme

---
+++ src/theme/colors.ts
@@ -0,0 +1,32 @@
+const colors = {
+  brandPrimary: "#1455D9",
+  brandSecondary: "#0D1440",
+  brandAccentPrimary: "#1438A6",
+  brandAccentSecondary: "#0E2773",
+  accentRed: "#BF1736",
+} as const
+
+/*
+  Color Theme Swatches in Hex
+  .TRIANGLED---Immersive-Light-Installation-1-hex { color: #BF1736; }
+  .TRIANGLED---Immersive-Light-Installation-2-hex { color: #0D1440; }
+  .TRIANGLED---Immersive-Light-Installation-3-hex { color: #1438A6; }
+  .TRIANGLED---Immersive-Light-Installation-4-hex { color: #0E2773; }
+  .TRIANGLED---Immersive-Light-Installation-5-hex { color: #1455D9; }
+
+  Color Theme Swatches in RGBA
+  .TRIANGLED---Immersive-Light-Installation-1-rgba { color: rgba(191, 22, 53, 1); }
+  .TRIANGLED---Immersive-Light-Installation-2-rgba { color: rgba(12, 20, 63, 1); }
+  .TRIANGLED---Immersive-Light-Installation-3-rgba { color: rgba(19, 56, 165, 1); }
+  .TRIANGLED---Immersive-Light-Installation-4-rgba { color: rgba(13, 39, 114, 1); }
+  .TRIANGLED---Immersive-Light-Installation-5-rgba { color: rgba(19, 85, 216, 1); }
+
+  Color Theme Swatches in HSLA
+  .TRIANGLED---Immersive-Light-Installation-1-hsla { color: hsla(348, 78, 42, 1); }
+  .TRIANGLED---Immersive-Light-Installation-2-hsla { color: hsla(230, 66, 15, 1); }
+  .TRIANGLED---Immersive-Light-Installation-3-hsla { color: hsla(225, 78, 36, 1); }
+  .TRIANGLED---Immersive-Light-Installation-4-hsla { color: hsla(225, 78, 25, 1); }
+  .TRIANGLED---Immersive-Light-Installation-5-hsla { color: hsla(219, 83, 46, 1); }
+*/
+
+export default colors
